2fdb1bd5d5df24c8aaad83ebda0e4ca7967e9ef6
Jerrod Dixon <jtdixon2015@gmail.com>
2016-06-14 16:28:17 -0500

    Query method

--- es_main.py
+++ es_main.py
@@ -1,15 +1,25 @@
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
+from string import rstrip
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
-es = Elasticsearch([
-    'urlhere'
-])
+with open("config", "r+") as txt:
+    contents = map(rstrip, txt)
 
-s = Search(using=es)
+host = contents[0]
+port = contents[1]
 
-ns = Search().using(es).query("match", AccountingGroup="production.cmsdataops")
+es = Elasticsearch([{
+    'host': host, 'port': port
+}])
 
-response = ns.execute()
+query={"query" : {"match" : {"Type" : "production"}}}
 
-for hit in ns:
-    print(hit.CoreHr)
+# response = ns.execute()
+scanner = es.search(index="_all", doc_type="job", body=query, search_type="scan", scroll="10m")
+scrollId = scanner['_scroll_id']
+
+response = es.scroll(scroll_id=scrollId, scroll="10m")
+pp.pprint(response)
+#for hit in response["hits"]["hits"]:
+#    print(hit)
