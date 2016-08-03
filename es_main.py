e01ddb17dd39aeb00be90fa069a804bc54c56f32
Jerrod Dixon <jtdixon2015@gmail.com>
2016-08-03 11:58:22 -0500

    Read full scroll

--- es_main.py
+++ es_main.py
@@ -22,7 +22,7 @@ es = Elasticsearch([{
     'host': host, 'port': port
 }])
 
-scrollPreserve="1m"
+scrollPreserve="3m"
 
 queryHCC={"query" : 
           {"bool": {
@@ -79,28 +79,43 @@ scannerAtlas = es.search(index="network_weather_2-*",
                          search_type="scan", 
                          scroll=scrollPreserve)
 scrollIdAtlas = scannerAtlas['_scroll_id']
-responseAtlas = es.scroll(scroll_id=scrollIdAtlas, 
-                          scroll=scrollPreserve)
+atlasTotalRec = scannerAtlas["hits"]["total"]
 dates = np.zeros(0)
 datesF = np.zeros(0)
 delayMean = np.zeros(0)
-# pp.pprint(response)
-for hit in responseAtlas["hits"]["hits"]:
-#    print(hit)
-    dates = np.append(dates, 
+delayMedian = np.zeros(0)
+delaySd = np.zeros(0)
+
+while atlasTotalRec > 0:
+    responseAtlas = es.scroll(scroll_id=scrollIdAtlas, 
+                              scroll=scrollPreserve)
+    for hit in responseAtlas["hits"]["hits"]:
+        dates = np.append(dates, 
                       dt.datetime.strptime(hit["_source"]["timestamp"], '%Y-%m-%dT%X'))
-    datesF = np.append(datesF, 
+        datesF = np.append(datesF, 
                        dt.datetime.strptime(hit["_source"]["timestamp"], '%Y-%m-%dT%X')
                           - timedelta(seconds=14400))
-    delayMean = np.append(delayMean, float(hit["_source"]["delay_mean"]))
-    print(hit)
+        delayMean = np.append(delayMean, float(hit["_source"]["delay_mean"]))
+        delayMedian = np.append(delayMedian, float(hit["_source"]["delay_median"]))
+        delaySd = np.append(delaySd, float(hit["_source"]["delay_sd"]))
+    atlasTotalRec -= len(responseAtlas['hits']['hits'])
 
 fig = plt.figure(figsize=(12, 6))
-hcms = fig.add_subplot(121)
-#hcms.plot(['delay_mean'])
-hcms.hlines(delayMean, datesF, dates)
-hcms.set_xlabel("Time Period")
-hcms.set_title("Delay Mean period")
+#hmean = fig.add_subplot(121)
+#hmean.plot(dates, delayMean, 'bs')
+#hmean.hlines(delayMean, datesF, dates)
+#hmean.set_xlabel("Time Period")
+#hmean.set_title("Delay Mean period")
+#hmedian = fig.add_subplot(122)
+#hmedian.plot(dates, delayMedian, 'gs')
+#hmedian.hlines(delayMedian, datesF, dates)
+#hmedian.set_xlabel("Time Period")
+#hmedian.set_title("Delay Median period")
+hsd = fig.add_subplot(111)
+hsd.plot(dates, delaySd, 'rs')
+hsd.hlines(delaySd, datesF, dates)
+hsd.set_xlabel("Time Period")
+hsd.set_title("Delay SD period")
 fig.autofmt_xdate()
 plt.show()
 plt.savefig(pp, format='pdf')
