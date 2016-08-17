e24751db393d43a3edba8c59cd1723971816aa11
Jerrod Dixon <jtdixon2015@gmail.com>
2016-08-17 16:51:21 -0500

    Change to Python3.0 api

--- es_main.py
+++ es_main.py
@@ -1,126 +1,257 @@
 from elasticsearch import Elasticsearch
-from string import rstrip
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
 import datetime as dt
 from datetime import timedelta
+from datetime import timezone
+import math
+import json
 
-# import pprint
-# pp = pprint.PrettyPrinter(indent=4)
-
-pp = PdfPages('CMS_Plots.pdf')
+with open('sites.json', 'r+') as txt:
+    sitesArray = json.load(txt)
 
 with open("config", "r+") as txt:
-    contents = map(rstrip, txt)
+    contents = list(map(str.rstrip, txt))
 
-host = contents[2]
-port = contents[3]
+esAtlas = Elasticsearch([{
+    'host': contents[2], 'port': contents[3]
+}], timeout=30)
 
-
-es = Elasticsearch([{
-    'host': host, 'port': port
-}])
+esHCC = Elasticsearch([{
+    'host': contents[0], 'port': contents[1]
+}], timeout=30)
 
 scrollPreserve="3m"
+startDate = "2016-07-17T00:00:00"
+endDate = "2016-07-23T00:00:00"
 
-queryHCC={"query" : 
-          {"bool": {
-              "must": [
-                  {"match" : 
-                     {"Type" : "production"}
-                  },
-                  {"range" :
-                     {"EventRate" : {"gte" : "0"}}
-                  },
-                  {"match" :
-                     {"DataLocationsCount" : 1}
-                  }
-              ]
+def conAtlasTime(time):
+    return dt.datetime.strptime(time, '%Y-%m-%dT%X')
+
+def atlasLatency(srcSite, destSite):
+    queryAtlas={"query" :
+                {"bool": {
+                   "must": [
+                     {"match" : 
+                         {"_type" : "latency" }
+                     },
+                     {"match" :
+                         {"srcSite" : srcSite }
+                     },
+                     {"match" :
+                         {"destSite" : destSite }
+                     },
+                     {"range" : {
+                         "timestamp" : {
+                             "gt" : startDate,
+                             "lt" : endDate
+                         }
+                     }}
+                   ]
+                 }
+                }
+               }
+    scannerAtlas = esAtlas.search(index="network_weather_2-*", 
+                                  body=queryAtlas, 
+                                  search_type="scan", 
+                                  scroll=scrollPreserve)
+    scrollIdAtlas = scannerAtlas['_scroll_id']
+    atlasTotalRec = scannerAtlas["hits"]["total"]
+    arrRet = {}
+    arrRet["dates"] = np.zeros(0)
+    arrRet["datesF"] = np.zeros(0)
+    arrRet["delay_mean"] = np.zeros(0)
+    arrRet["delay_median"] = np.zeros(0)
+    arrRet["delay_sd"] = np.zeros(0)
+
+    while atlasTotalRec > 0:
+        responseAtlas = esAtlas.scroll(scroll_id=scrollIdAtlas, 
+                                       scroll=scrollPreserve)
+        for hit in responseAtlas["hits"]["hits"]:
+            arrRet["dates"] = np.append(arrRet["dates"], 
+                              conAtlasTime(hit["_source"]["timestamp"]))
+            arrRet["datesF"] = np.append(arrRet["datesF"], 
+                               conAtlasTime(hit["_source"]["timestamp"])
+                                 - timedelta(seconds=14400))
+        #throughPut = np.append(throughPut, float(hit["_source"]["throughput"]) / math.pow(1000,3))
+            arrRet["delay_mean"] = np.append(arrRet["delay_mean"], float(hit["_source"]["delay_mean"]))
+            arrRet["delay_median"] = np.append(arrRet["delay_median"], float(hit["_source"]["delay_median"]))
+            arrRet["delay_sd"] = np.append(arrRet["delay_sd"], float(hit["_source"]["delay_sd"]))
+
+        atlasTotalRec -= len(responseAtlas['hits']['hits'])
+    es.clear_scroll(scroll_id=scrollIdAtlas)
+    return arrRet
+
+
+def atlasPacketLoss(srcSite, destSite):
+    queryAtlas={"query" :
+                {"bool": {
+                   "must": [
+                     {"match" : 
+                         {"_type" : "packet_loss_rate"}
+                     },
+                     {"match" :
+                         {"srcSite" : srcSite }
+                     },
+                     {"match" :
+                         {"destSite" : destSite }
+                     },
+                     {"range" : {
+                         "timestamp" : {
+                             "gt" : startDate,
+                             "lt" : endDate
+                         }
+                     }}
+                   ]
+                 }
+                }
+               }
+    scannerAtlas = esAtlas.search(index="network_weather_2-*", 
+                                  body=queryAtlas, 
+                                  search_type="scan", 
+                                  scroll=scrollPreserve)
+    scrollIdAtlas = scannerAtlas['_scroll_id']
+    atlasTotalRec = scannerAtlas["hits"]["total"]
+    arrRet = {}
+    arrRet["dates"] = np.zeros(0)
+    arrRet["datesF"] = np.zeros(0)
+    arrRet["packet_loss"] = np.zeros(0)
+
+    while atlasTotalRec > 0:
+        responseAtlas = esAtlas.scroll(scroll_id=scrollIdAtlas, 
+                                       scroll=scrollPreserve)
+        for hit in responseAtlas["hits"]["hits"]:
+            arrRet["dates"] = np.append(arrRet["dates"], 
+                              conAtlasTime(hit["_source"]["timestamp"]))
+            arrRet["datesF"] = np.append(arrRet["datesF"], 
+                               conAtlasTime(hit["_source"]["timestamp"])
+                                 - timedelta(seconds=14400))
+        #throughPut = np.append(throughPut, float(hit["_source"]["throughput"]) / math.pow(1000,3))
+            arrRet["packet_loss"] = np.append(arrRet["packet_loss"], float(hit["_source"]["packet_loss"]))
+
+        atlasTotalRec -= len(responseAtlas['hits']['hits'])
+    es.clear_scroll(scroll_id=scrollIdAtlas)
+    return arrRet
+
+def atlasThroughput(srcSite, destSite):
+    queryAtlas={"query" :
+                {"bool": {
+                   "must": [
+                     {"match" : 
+                         {"_type" : "throughput"}
+                     },
+                     {"match" :
+                         {"srcSite" : srcSite }
+                     },
+                     {"match" :
+                         {"destSite" : destSite }
+                     },
+                     {"range" : {
+                         "timestamp" : {
+                             "gt" : startDate,
+                             "lt" : endDate
+                         }
+                     }}
+                   ]
+                 }
+                }
+               }
+    scannerAtlas = esAtlas.search(index="network_weather_2-*", 
+                                  body=queryAtlas, 
+                                  search_type="scan", 
+                                  scroll=scrollPreserve)
+    scrollIdAtlas = scannerAtlas['_scroll_id']
+    atlasTotalRec = scannerAtlas["hits"]["total"]
+    arrRet = {}
+    arrRet["dates"] = np.zeros(0)
+    arrRet["datesF"] = np.zeros(0)
+    arrRet["throughput"] = np.zeros(0)
+
+    while atlasTotalRec > 0:
+        responseAtlas = esAtlas.scroll(scroll_id=scrollIdAtlas, 
+                                       scroll=scrollPreserve)
+        for hit in responseAtlas["hits"]["hits"]:
+            arrRet["dates"] = np.append(arrRet["dates"], 
+                              conAtlasTime(hit["_source"]["timestamp"]))
+            arrRet["datesF"] = np.append(arrRet["datesF"], 
+                               conAtlasTime(hit["_source"]["timestamp"])
+                                 - timedelta(seconds=14400))
+        #throughPut = np.append(throughPut, float(hit["_source"]["throughput"]) / math.pow(1000,3))
+            arrRet["throughput"] = np.append(arrRet["throughput"], float(hit["_source"]["throughput"]))
+
+        atlasTotalRec -= len(responseAtlas['hits']['hits'])
+    es.clear_scroll(scroll_id=scrollIdAtlas)
+    return arrRet
+
+def hccQuery(site):
+    queryHCC={"query" : 
+              {"bool": {
+                  "must": [
+                      {"match" : 
+                         {"Type" : "production"}
+                      },
+                      {"range" :
+                         {"EventRate" : {"gte" : "0"}}
+                      },
+                      {"range" : {
+                         "CompletionDate" : {
+                             "gt" : conAtlasTime(startDate),
+                             "lt" : conAtlasTime(endDate)
+                         }
+                      }},
+                      {"match" :
+                         {"DataLocationsCount" : 1}
+                      },
+                      {"match" : 
+                         {"Site" : site }
+                      },
+                      {"match" :
+                         {"InputData" : "Offsite"}
+                      }
+                  ]
+              }
           }
       }
-  }
 
-queryAtlas={"query" :
-              {"bool": {
-                 "must": [
-                   {"match" : 
-                       #{"_type" : "packet_loss_rate"}
-                       {"_type" : "latency" }
-                   },
-                   {"match" :
-                       {"srcSite" : "RO-16-UAIC" }
-                   },
-                   {"match" :
-                       {"destSite" : "AGLT2" }
-                   },
-                   {"range" : {
-                       "timestamp" : {
-                           "gt" : "2016-07-31T00:00:00",
-                           "lt" : "2016-08-01T00:00:00"
-                       }
-                   }}
-                 ]
-               }
-              }
-           }
+    scannerHCC = es.search(index="cms-*", 
+                           doc_type="job", 
+                           body=queryHCC, 
+                           search_type="scan", 
+                           scroll=scrollPreserve)
+    scrollIdHCC = scannerHCC['_scroll_id']
+    countHitsHCC = scannerHCC["hits"]["total"]
+    arrRet = {}
 
-#scannerHCC = es.search(index="_all", 
-#                       doc_type="job", 
-#                       body=queryHCC, 
-#                       search_type="scan", 
-#                       scroll=scrollPreserve)
-#scrollIdHCC = scannerHCC['_scroll_id']
-#responseHCC = es.scroll(scroll_id=scrollIdHCC, 
-#                     scroll=scrollPreserve)
+    while countHitsHCC > 0:
+        responseHCC = es.scroll(scroll_id=scrollIdHCC, 
+                                scroll=scrollPreserve)
+        for hit in responseHCC["hits"]["hits"]:
+            print("placeholder")
+        countHitsHCC -= len(responseHCC['hits']['hits'])
+    es.clear_scroll(scroll_id=scrollIdHCC)
+    return arrRet
 
-scannerAtlas = es.search(index="network_weather_2-*", 
-                         body=queryAtlas, 
-                         search_type="scan", 
-                         scroll=scrollPreserve)
-scrollIdAtlas = scannerAtlas['_scroll_id']
-atlasTotalRec = scannerAtlas["hits"]["total"]
-dates = np.zeros(0)
-datesF = np.zeros(0)
-delayMean = np.zeros(0)
-delayMedian = np.zeros(0)
-delaySd = np.zeros(0)
+print(conAtlasTime(startDate).replace(tzinfo=timezone.utc).timestamp())
 
-while atlasTotalRec > 0:
-    responseAtlas = es.scroll(scroll_id=scrollIdAtlas, 
-                              scroll=scrollPreserve)
-    for hit in responseAtlas["hits"]["hits"]:
-        dates = np.append(dates, 
-                      dt.datetime.strptime(hit["_source"]["timestamp"], '%Y-%m-%dT%X'))
-        datesF = np.append(datesF, 
-                       dt.datetime.strptime(hit["_source"]["timestamp"], '%Y-%m-%dT%X')
-                          - timedelta(seconds=14400))
-        delayMean = np.append(delayMean, float(hit["_source"]["delay_mean"]))
-        delayMedian = np.append(delayMedian, float(hit["_source"]["delay_median"]))
-        delaySd = np.append(delaySd, float(hit["_source"]["delay_sd"]))
-    atlasTotalRec -= len(responseAtlas['hits']['hits'])
+#arrayAtlas = atlasPacketLoss(queryAtlas)
+#fig = plt.figure(figsize=(12, 6))
 
-fig = plt.figure(figsize=(12, 6))
-#hmean = fig.add_subplot(121)
-#hmean.plot(dates, delayMean, 'bs')
-#hmean.hlines(delayMean, datesF, dates)
-#hmean.set_xlabel("Time Period")
-#hmean.set_title("Delay Mean period")
-#hmedian = fig.add_subplot(122)
-#hmedian.plot(dates, delayMedian, 'gs')
-#hmedian.hlines(delayMedian, datesF, dates)
-#hmedian.set_xlabel("Time Period")
-#hmedian.set_title("Delay Median period")
-hsd = fig.add_subplot(111)
-hsd.plot(dates, delaySd, 'rs')
-hsd.hlines(delaySd, datesF, dates)
-hsd.set_xlabel("Time Period")
-hsd.set_title("Delay SD period")
-fig.autofmt_xdate()
-plt.show()
-plt.savefig(pp, format='pdf')
+#hput = fig.add_subplot(111)
+#hput.plot(arrayAtlas["dates"], arrayAtlas["packetLoss"], 'bs')
+#hput.hlines(arrayAtlas["packetLoss"], arrayAtlas["datesF"], arrayAtlas["dates"])
+#hput.set_xlabel("Time Period")
+#hput.set_title("Packet Loss")
+#fig.autofmt_xdate()
+#plt.show()
 
-#es.clear_scroll(scroll_id=scrollIdAtlas)
+#with PdfPages('CMS_Plots.pdf') as pp:
+#    d = pp.infodict()
+#    d['Title'] = 'CMS Grid Plots'
+#    d['Author'] = u'Jerrod T. Dixon\xe4nen'
+#    d['Subject'] = 'Plot of network affects on grid jobs'
+#    d['Keywords'] = 'PdfPages matplotlib CMS grid'
+#    d['CreationDate'] = dt.datetime.today()
+#    d['ModDate'] = dt.datetime.today()
 
-# Create trace for scatter plot
-
+#    pp.savefig(fig)
